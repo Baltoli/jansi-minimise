ed3a4c6aba0e4a79e0ab0bad0e54101e04037965
Bruce Collie <brucecollie82@gmail.com>
2021-11-10 15:10:49 +0000

    Compare

--- src/main/java/org/kframework/test/App.java
+++ src/main/java/org/kframework/test/App.java
@@ -1,14 +1,12 @@
 package org.kframework.test;
 
-import org.kframework.mpfr.BigFloat;
-import org.kframework.mpfr.BinaryMathContext;
-
 /**
  * Hello world!
  *
  */
 public class App {
   public static void main(String[] args) {
-    System.out.println(Math.exp(1.0));
+    System.out.println("Strict : " + StrictMath.exp(1.0));
+    System.out.println("Default: " + Math.exp(1.0));
   }
 }
